d0289964b3751fa6937e25b898198a23f940821c
dinnu006 <dineshavisetti@gmail.com>
2022-07-31 14:03:31 +0530

    End of section  2

---
+++ Walks/Walks.API/Data/WalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Walks.API.Models.Domains;
+
+namespace Walks.API.Data
+{
+    public class WalksDbContext:DbContext
+    {
+        public WalksDbContext(DbContextOptions<WalksDbContext> option):base(option)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ Walks/Walks.API/Models/Domains/WalkDifficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace Walks.API.Models.Domains
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
